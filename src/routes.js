2ba951584b52ca11a9e51a91fa08fb65a5647daa
GabrielFraga <gabrielgoog@gmail.com>
2019-10-31 20:55:57 -0300

    mongodb para armazenamento de notificacoes

--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,9 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
-
+import ScheduelController from './app/controllers/ScheduleController';
 import FileController from './app/controllers/FileController';
+import NotificationController from './app/controllers/NotificationController';
 
 import authMiddeware from './app/middewares/auth';
 
@@ -23,7 +24,12 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduelController.index);
+
+routes.get('/notifications', NotificationController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
